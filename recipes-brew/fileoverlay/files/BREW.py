d4794516e89b1a4a2bb5ab2d99834530b6dca1b3
jim <spammailhere99@yahoo.com>
2017-11-29 21:18:15 -0600

    System is on better/more accurate timers. 1s resolution now

--- recipes-brew/fileoverlay/files/BREW.py
+++ recipes-brew/fileoverlay/files/BREW.py
@@ -1,50 +1,59 @@
 import os
 import time
 import threading
+from subprocess import call
 from flask import Flask, render_template, request, redirect, url_for
 from PIDBrewLoop import PIDBrewLoop
 
 
 ######WEB ACTIVITIES###########
 #Note: Since the webpage is set to refresh every 10s,
-#	a post or get request should not be sent more than once,
-#	so after a request, the page is redirected to itself (no request so refresh doesn't re-request)
+#       a post or get request should not be sent more than once,
+#       so after a request, the page is redirected to itself (no request so refresh doesn't re-request)
 app = Flask(__name__)
 
 @app.route("/", methods=['POST','GET']) #the home page
 def BrewMe():
 
-	#button looks like <input type="submit" value="Submit" name="changeSetPT">
-	if (request.args.get('changeSetPT') == 'Submit') :
-		BrewLoop.SetTemperatureSetPt(float(request.args.get('newSetPT')))
-		return redirect(url_for('BrewMe'))
+    #button looks like <input type="submit" value="Submit" name="changeSetPT">
+    if (request.args.get('changeSetPT') == 'Submit') :
+        BrewLoop.SetTemperatureSetPt(float(request.args.get('newSetPT')))
+        return redirect(url_for('BrewMe'))
 
-	#button looks like <input type="submit" name="stopHeat" value="Turn Off Heat">
-	if (request.args.get('stopHeat') == 'Turn Off Heat') :
-		BrewLoop.SetTemperatureSetPt(0)
-		return redirect(url_for('BrewMe'))
+    #button looks like <input type="submit" name="stopHeat" value="Turn Off Heat">
+    if (request.args.get('stopHeat') == 'Turn Off Heat') :
+        BrewLoop.SetTemperatureSetPt(0)
+        return redirect(url_for('BrewMe'))
 
-	#button looks like <input type="submit" name="boil" Value="Boil">
-	if (request.args.get('boil') == 'Boil'):
-		BrewLoop.SetTemperatureSetPt(100)
-		return redirect(url_for('BrewMe'))
-	else:
-		pass #unknown request attempted
+    #button looks like <input type="submit" name="boil" Value="Boil">
+    if (request.args.get('boil') == 'Boil') :
+        BrewLoop.SetTemperatureSetPt(100)
+        return redirect(url_for('BrewMe'))
+    else:
+        pass #unknown request attempted
 
-	#variables available in HTML
+    #variables available in HTML
     BrewLoop.lock.acquire()
-	templateData = {
-	    'tempData'      : BrewLoop.TempHistory,
-	    'setpt'         : str(BrewLoop.TemperatureSetPt),
-	    'vRegSetPt'     : str(BrewLoop.VRegSetPt),
-	}
+    templateData = {
+       'tempData'   : BrewLoop.TempHistory,
+       'setpt'      : str(BrewLoop.TemperatureSetPt),
+       'vRegSetPt'  : str(BrewLoop.CurVRegSetPt),
+    }
     BrewLoop.lock.release()
-	return render_template('BrewMe.html', **templateData)
+    return render_template('BrewMe.html', **templateData)
+
+def CheckNetwork():
+    # Repeat every 30 seconds
+    threading.Timer(30, CheckNetwork).start()
+
+    # Check that the network is still up
+    call(["/brewpi/restartNetwork.sh"])
 
 
 ########Start Threads#############
 BrewLoop = PIDBrewLoop()
 BrewLoop.start()
+CheckNetwork()
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=80, debug=True, use_reloader=False)
--- recipes-brew/fileoverlay/files/PIDBrewLoop.py
+++ recipes-brew/fileoverlay/files/PIDBrewLoop.py
@@ -1,7 +1,6 @@
 import time
 import threading
 import logging
-from subprocess import call
 from HardwareUtility import HardwareUtility
 from PID import PID
 
@@ -13,67 +12,86 @@ class PIDBrewLoop(threading.Thread):
         self.lock = threading.Lock()
 
         self.Hardware = HardwareUtility()
-        self.TempHistory = [0]*359       #Used in Web Graphs
+        self.TempHistory = [0]*3599         #Used in Web Graphs
         self.TemperatureSetPt = 0           #Set Point in Celcius
         self.RestartPID = True              #Use when changing Set Point
-        self.VRegSetPt = 0                  #Voltage Regulator Set Point (0-100, capped at 55)
+        self.CurVRegSetPt = 0               #What the Voltage Regulator is actually set to (0-100)
+        self.NewVRegSetPt = 0               #What the Voltage Regulator should be set to
         self.MaxVRegSetPt = 65              #Highest alowable setpoint for voltage regulator
         self.PidControl = PID(40, 0.2, 1)   #P:100 I:0.2 D:0 for quick heat
         self.PidControl.setPoint(0)
 
+        self.CollectTemp()
+        self.ComputeVoltageRegulatorSetpt()
 
-    def run(self):
-        self.SetVRegTo = 0
+    def CollectTemp(self):
+        # Repeat every second
+        threading.Timer(1, self.CollectTemp).start()
+
+        # Read temperature sensors
+        self.lock.acquire()
+        self.TempHistory.insert(0, self.Hardware.read_temp())
+        self.TempHistory.pop()
+        self.lock.release()
+
+    def ComputeVoltageRegulatorSetpt(self):
+        # Repeat every second
+        threading.Timer(1, self.ComputeVoltageRegulatorSetpt).start()
+
         self.lock.acquire()
 
-        while True:
+        # Temperature set point is 0 Celcius (or below). Turn off heating element
+        if self.TemperatureSetPt == 0:
+            self.NewVRegSetPt = 0
 
-            # Check that the network is still up
-            call(["/brewpi/restartNetwork.sh"])
-
-            # Read temperature sensors
-            self.TempHistory.insert(0, self.Hardware.read_temp())
-            self.TempHistory.pop()
-
-            # Temperature set point is 0 Celcius (or below). Turn off heating element
-            if self.TemperatureSetPt == 0:
-                self.SetVRegTo = 0
-
-            #Boil Mode (No PID). Manually set voltage regualtor
-            elif self.TemperatureSetPt == 100:
-                if self.TempHistory[0] < 95 :
-                    self.SetVRegTo = self.MaxVRegSetPt;
-                else:
-                    self.SetVRegTo = 35
-
-            #Set Point Mode (PID Algorithm)
+        #Boil Mode (No PID). Manually set voltage regualtor
+        elif self.TemperatureSetPt == 100:
+            if self.TempHistory[0] < 95 :
+                self.NewVRegSetPt = self.MaxVRegSetPt
             else:
-                #if the setpoint has changed, restart PID
-                if self.RestartPID:
-                    self.PidControl.setPoint(self.TemperatureSetPt)
-                self.SetVRegTo = self.PidControl.update(self.TempHistory[0])
-                self.SetVRegTo = int(self.SetVRegTo)
+                self.NewVRegSetPt = 35
 
-            #Set the voltage regulator with the calculated value
-            print("SetVRegTo: " + str(self.SetVRegTo))
-            while self.VRegSetPt > self.SetVRegTo and self.VRegSetPt > 0 and self.VRegSetPt < self.MaxVRegSetPt+1:
-                self.Hardware.dec_temp()
-                self.VRegSetPt -= 1
-            while self.VRegSetPt < self.SetVRegTo and self.VRegSetPt > -1 and self.VRegSetPt < self.MaxVRegSetPt:
-                self.Hardware.inc_temp()
-                self.VRegSetPt +=1
-            print("VReg: " + str(self.VRegSetPt))
+        #Set Point Mode (PID Algorithm)
+        else:
+            #if the setpoint has changed, restart PID
+            if self.RestartPID:
+                self.PidControl.setPoint(self.TemperatureSetPt)
+            self.NewVRegSetPt = self.PidControl.update(self.TempHistory[0])
+            self.NewVRegSetPt = int(self.NewVRegSetPt)
 
-            self.lock.release()
+        self.lock.release()
 
-            # Always sleep for 10s between loop iterations
-            time.sleep(10)
 
+    def run(self):
+
+        # Actualize the voltage regulator setpoint every 1 second.
+        # Do not use a timer because this may take longer than
+        # 1 second to run.
+        while(True):
             self.lock.acquire()
+            unlockedNewVRegSetPt = self.NewVRegSetPt
+            unlockedCurVRegSetPt = self.CurVRegSetPt
+            self.lock.release()
+    
+            #Set the voltage regulator with the calculated value
+            print("NewVRegSetPt: " + str(unlockedNewVRegSetPt))
+            while unlockedCurVRegSetPt > unlockedNewVRegSetPt and unlockedCurVRegSetPt > 0 and unlockedCurVRegSetPt < self.MaxVRegSetPt+1:
+                self.Hardware.dec_temp()
+                unlockedCurVRegSetPt -= 1
+            while unlockedCurVRegSetPt < unlockedNewVRegSetPt and unlockedCurVRegSetPt > -1 and unlockedCurVRegSetPt < self.MaxVRegSetPt:
+                self.Hardware.inc_temp()
+                unlockedCurVRegSetPt +=1
+            print("VReg: " + str(unlockedCurVRegSetPt))
+    
+            self.lock.acquire()
+            self.NewVRegSetPt = unlockedNewVRegSetPt
+            self.CurVRegSetPt = unlockedCurVRegSetPt 
+            self.lock.release()
+            time.sleep(1)
 
 
     def SetTemperatureSetPt(self, temp):
-      	self.lock.acquire()
+        self.lock.acquire()
         self.TemperatureSetPt = temp
         self.RestartPID = True
         self.lock.release()
